a28582b77ca624ecbe2c1784577abd4beb19c64b
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-10-05 13:23:51 -0400

    Create TestIntake.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestIntake.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name = "Test Intake", group = "Testing")
+public class TestIntake extends OpMode {
+
+    public void init() {
+
+    }
+
+    public void loop() {
+    }
+
+}
