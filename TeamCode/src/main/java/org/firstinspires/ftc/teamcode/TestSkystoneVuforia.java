0c6e8f014621f44623bdddc54eed411dbfa01a47
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-10-13 14:10:09 -0400

    Added TestSkystoneVuforia test OpMode
    We will be using this OpMode to determine the randomization pattern of the Skystones.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestSkystoneVuforia.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name = "Skystone Locator test", group = "Testing")
+public class TestSkystoneVuforia extends LinearOpMode {
+
+    public void runOpMode() {
+        
+    }
+}
