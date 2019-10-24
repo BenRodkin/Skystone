24aea2d51d49124ccebba34aee2439fca3b9d4b6
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-10-24 17:14:15 -0400

    Copied in PID methods and telemetry from RoverRuckus repository

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestPID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestPID.java
@@ -44,9 +44,64 @@ public class TestPID extends LinearOpMode {
 
 
         while(opModeIsActive()) {
+            telemetry.addData("kP", hardware.pid.getP());
+            telemetry.addData("kI", hardware.pid.getI());
+            telemetry.addData("kD", hardware.pid.getD());
+            telemetry.addLine();
+            telemetry.addData("Heading", heading());
+            telemetry.update();
         }
 
 
+    }
 
+    //----------------------------------------------------------------------------------------------
+    // PID controller methods
+    //----------------------------------------------------------------------------------------------
+
+    public void turnToHeadingPID(int target) throws InterruptedException {
+
+        telemetry.addData("Turning to target", target);
+        telemetry.addLine("Press dpad_down to stop.");
+
+        hardware.pid.setSetpoint(target);                                       // Set target final heading relative to current
+        hardware.pid.setOutputRange(-hardware.MAX_SPEED, hardware.MAX_SPEED);   // Set maximum motor power
+        hardware.pid.setDeadband(hardware.TOLERANCE);                           // Set how far off you can safely be from your target
+
+        while (opModeIsActive()) {
+            double error = normalize180(target - heading());
+            double power = hardware.pid.calculateGivenError(error);
+
+            telemetry.addData("Current error", error);
+            telemetry.addData("Current power", power);
+
+            hardware.setLeftPower(-power);
+            hardware.setRightPower(power);
+
+            if (Math.abs(error) < hardware.TOLERANCE || gamepad2.dpad_down) {
+                break;
+            }
+
+            Thread.sleep(1);
+
+            telemetry.update();
+        }
+
+        hardware.setLeftPower(0);
+        hardware.setRightPower(0);
+    }
+
+    public double normalize180(double angle) {
+        while(angle > 180) {
+            angle -= 360;
+        }
+        while(angle <= -180) {
+            angle += 360;
+        }
+        return angle;
+    }
+
+    public float heading() {
+        return imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle;
     }
 }
