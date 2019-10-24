8cfce18a3fb02dfae7361b9af97351a896f2764b
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-10-24 17:13:53 -0400

    Copied in imu and hardware from RoverRuckus repository

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestPID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestPID.java
@@ -1,12 +1,52 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+
 
 @Autonomous(name = "Test: SynchronousPID", group = "Testing")
 public class TestPID extends LinearOpMode {
 
+    // IMU
+    BNO055IMU imu;
+
+    // Hardware class
+    SLICBotHardware hardware = new SLICBotHardware();
+
     public void runOpMode() {
-        
+
+        telemetry.addLine("Initializing hardware");
+        telemetry.update();
+
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
+        hardware.init(hardwareMap);
+
+        telemetry.addLine("Ready");
+        telemetry.update();
+
+        waitForStart();
+
+
+        while(opModeIsActive()) {
+        }
+
+
+
     }
 }
