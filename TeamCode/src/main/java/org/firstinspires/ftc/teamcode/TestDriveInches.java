de6aae6237a3c9611f8695da7ecce19ab1976712
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-10-23 22:36:45 -0400

    Created TestDriveInches

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDriveInches.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name = "Test: driveInches()", group = "Testing")
+public class TestDriveInches extends LinearOpMode {
+
+
+    public void runOpMode() {
+        while(opModeIsActive()) {
+        }
+
+    }
+}
