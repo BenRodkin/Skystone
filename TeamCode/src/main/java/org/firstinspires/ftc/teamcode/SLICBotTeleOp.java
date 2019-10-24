b7d6edca2239e6331b1de7090d3f56ac89f50c4b
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-10-23 22:31:44 -0400

    Replaced hardware initialization with SLICBotHardware initialization

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SLICBotTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SLICBotTeleOp.java
@@ -9,94 +9,15 @@ import com.qualcomm.robotcore.hardware.Servo;
 @TeleOp(name = "Driver Control", group = "TeleOp")
 public class SLICBotTeleOp extends OpMode {
 
-    public DcMotor frontLeft;
-    public DcMotor frontRight;
-    public DcMotor rearLeft;
-    public DcMotor rearRight;
-
-    public DcMotor intake1;
-    public DcMotor intake2;
-
-    public DcMotor pulley;
-
-    public DcMotor arm;
-
-    public Servo deploy1;
-    public Servo deploy2;
-
-    public Servo clamp;
-
-    public Servo holder;
-
-    // Deploy servo positions (index 0 is the left servo, index 1 is the right)
-    public final double[] STOWED =      {0.0, 0.0};
-    public final double[] DEPLOYED =    {1.0, 1.0};
-    public final double[] GRABBING =    {0.5, 0.4};
-
-    public final boolean BRAKE_ON_ZERO = true;
-
-    // Speed modifiers
-    public final double SLOW = 0.4;
-    public final double NORMAL = 0.8;
-    public final double FAST = 1.0;
-
-    public double driverSpeedMod = NORMAL;
-
-
+    SLICBotHardware hardware = new SLICBotHardware();
 
     // Button cooldowns
     ButtonCooldown gp2_a = new ButtonCooldown();
     ButtonCooldown gp2_x = new ButtonCooldown();
 
     public void init() {
-        frontLeft   = hardwareMap.dcMotor.get("fl_drive");
-        frontRight  = hardwareMap.dcMotor.get("fr_drive");
-        rearLeft    = hardwareMap.dcMotor.get("rl_drive");
-        rearRight   = hardwareMap.dcMotor.get("rr_drive");
 
-
-        intake1     = hardwareMap.dcMotor.get("intake1");
-        intake2     = hardwareMap.dcMotor.get("intake2");
-
-        pulley      = hardwareMap.dcMotor.get("pulley");
-
-        arm         = hardwareMap.dcMotor.get("arm");
-
-        deploy1     = hardwareMap.servo.get("deploy1");
-        deploy2     = hardwareMap.servo.get("deploy2");
-
-        clamp       = hardwareMap.servo.get("clamp");
-
-        holder      = hardwareMap.servo.get("holder");
-
-
-
-        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        rearLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-
-
-        intake2.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        pulley.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        arm.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        if(BRAKE_ON_ZERO) {
-            frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            rearLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            rearRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        } else {
-            frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-            frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-            rearLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-            rearRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-
-        }
-
+        hardware.init(hardwareMap);
         gp2_a.setCooldown(1.000);
         gp2_x.setCooldown(1.000);
 
