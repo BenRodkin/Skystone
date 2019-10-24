b2d79ba033753c20aa464b5be5d902b496a0228f
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-10-23 22:37:13 -0400

    Added hardware class and drive encoder telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDriveInches.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDriveInches.java
@@ -6,9 +6,25 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 @Autonomous(name = "Test: driveInches()", group = "Testing")
 public class TestDriveInches extends LinearOpMode {
 
+    SLICBotHardware hardware = new SLICBotHardware();
 
     public void runOpMode() {
+
+        hardware.init(hardwareMap);
+
+        telemetry.addLine("Ready");
+        telemetry.update();
+
+        waitForStart();
+
         while(opModeIsActive()) {
+
+
+            telemetry.addData("FL encoder", hardware.frontLeft.getCurrentPosition());
+            telemetry.addData("FR encoder", hardware.frontRight.getCurrentPosition());
+            telemetry.addData("RL encoder", hardware.rearLeft.getCurrentPosition());
+            telemetry.addData("RR encoder", hardware.rearRight.getCurrentPosition());
+            telemetry.update();
         }
 
     }
