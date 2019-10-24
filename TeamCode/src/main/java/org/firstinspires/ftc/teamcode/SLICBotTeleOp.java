510c4ab3f982fccfef3597c302cc564561ec590a
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-10-23 22:33:20 -0400

    Updated to utilize hardware class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SLICBotTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SLICBotTeleOp.java
@@ -30,9 +30,9 @@ public class SLICBotTeleOp extends OpMode {
 
 
         // Speed Modifiers
-        if(gamepad1.left_bumper) driverSpeedMod = SLOW;
-        else if(gamepad1.right_bumper) driverSpeedMod = FAST;
-        else driverSpeedMod = NORMAL;
+        if(gamepad1.left_bumper) hardware.driverSpeedMod = hardware.SLOW;
+        else if(gamepad1.right_bumper) hardware.driverSpeedMod = hardware.FAST;
+        else hardware.driverSpeedMod = hardware.NORMAL;
 
 
 
@@ -42,29 +42,29 @@ public class SLICBotTeleOp extends OpMode {
         double turn = gamepad1.left_stick_x * 3 / 4;
 
         if(drive > 0.0) {
-            setLeftPower((drive + turn) * driverSpeedMod);
-            setRightPower((drive - turn) * driverSpeedMod);
+            hardware.setLeftPower((drive + turn) * hardware.driverSpeedMod);
+            hardware.setRightPower((drive - turn) * hardware.driverSpeedMod);
         } else {
-            setLeftPower((drive - turn) * driverSpeedMod);
-            setRightPower((drive + turn) * driverSpeedMod);
+            hardware.setLeftPower((drive - turn) * hardware.driverSpeedMod);
+            hardware.setRightPower((drive + turn) * hardware.driverSpeedMod);
         }
 
         double pulleySpeed = (gamepad2.right_trigger - gamepad2.left_trigger);
-        telemetry.addData("motor position", pulley.getCurrentPosition());
+        telemetry.addData("motor position", hardware.pulley.getCurrentPosition());
 
-        pulley.setPower(pulleySpeed);
+        hardware.pulley.setPower(pulleySpeed);
 
-        arm.setPower(gamepad2.left_stick_y * 0.4);
+        hardware.arm.setPower(gamepad2.left_stick_y * 0.4);
 
         double runtime = getRuntime();
 
         if (gamepad2.a && gp2_a.ready(runtime)) {
-            clamp.setPosition(Math.abs(clamp.getPosition() - 1));
+            hardware.clamp.setPosition(Math.abs(hardware.clamp.getPosition() - 1));
             gp2_a.updateSnapshot(runtime);
         }
 
         if (gamepad2.x && gp2_x.ready(runtime)) {
-            holder.setPosition((Math.abs(holder.getPosition() - 1)));
+            hardware.holder.setPosition((Math.abs(hardware.holder.getPosition() - 1)));
             gp2_x.updateSnapshot(runtime);
         }
 
@@ -78,41 +78,30 @@ public class SLICBotTeleOp extends OpMode {
 
         // Deploy servo controls
         if(gamepad2.dpad_down) {        // STOWED
-            deploy2.setPosition(STOWED[0]);
-            deploy1.setPosition(STOWED[1]);
+            hardware.deploy2.setPosition(hardware.STOWED[0]);
+            hardware.deploy1.setPosition(hardware.STOWED[1]);
         } else if(gamepad2.dpad_left) { // DEPLOYED
-            deploy2.setPosition(DEPLOYED[0]);
-            deploy1.setPosition(DEPLOYED[1]);
+            hardware.deploy2.setPosition(hardware.DEPLOYED[0]);
+            hardware.deploy1.setPosition(hardware.DEPLOYED[1]);
         } else if(gamepad2.dpad_up) {   // GRABBING
-            deploy2.setPosition(GRABBING[0]);
-            deploy1.setPosition(GRABBING[1]);
+            hardware.deploy2.setPosition(hardware.GRABBING[0]);
+            hardware.deploy1.setPosition(hardware.GRABBING[1]);
         }
 
 
         telemetry.addData("Drive power", drive);
         telemetry.addData("Turn power", turn);
         telemetry.addLine();
-        telemetry.addData("Intake1 power", intake1.getPower());
-        telemetry.addData("Intake2 power", intake2.getPower());
+        telemetry.addData("Intake1 power", hardware.intake1.getPower());
+        telemetry.addData("Intake2 power", hardware.intake2.getPower());
         telemetry.addLine();
-        telemetry.addData("Deploy1 position",deploy1.getPosition());
-        telemetry.addData("Deploy2 position",deploy2.getPosition());
+        telemetry.addData("Deploy1 position", hardware.deploy1.getPosition());
+        telemetry.addData("Deploy2 position", hardware.deploy2.getPosition());
         telemetry.update();
     }
 
-    public void setLeftPower(double power) {
-        rearLeft.setPower(power);
-        frontLeft.setPower(power);
-
-    }
-
-    public void setRightPower(double power) {
-        rearRight.setPower(power);
-        frontRight.setPower(power);
-    }
-
     public void setIntakePower(double power) {
-        intake1.setPower(power);
-        intake2.setPower(power);
+        hardware.intake1.setPower(power);
+        hardware.intake2.setPower(power);
     }
 }
