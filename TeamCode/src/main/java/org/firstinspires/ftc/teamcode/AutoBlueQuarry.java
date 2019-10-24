1f8ef8c56f1fbbb13618f4d6f8c54b49fb1301dc
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-10-24 16:53:53 -0400

    Added encoder usage methods and hardware instance

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBlueQuarry.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBlueQuarry.java
@@ -2,11 +2,87 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 @Autonomous(name = "Blue Quarry", group = "Autonomous")
 public class AutoBlueQuarry extends LinearOpMode {
 
+    SLICBotHardware hardware = new SLICBotHardware();
+
+
     public void runOpMode() {
+
+        hardware.init(hardwareMap);
+
+        telemetry.addLine("Ready");
+        telemetry.update();
+
+        waitForStart();
+        while(opModeIsActive()) {
+            telemetry.addLine("Finished.");
+            telemetry.update();
+        }
+
+    }
+
+
+
+
+
+    public void moveArmCounts(int counts, double speed) {
+        hardware.arm.setTargetPosition (hardware.arm.getCurrentPosition() + counts);
+
+        hardware.arm.setMode  (DcMotor.RunMode.RUN_TO_POSITION);
+
+        hardware.arm.setPower(speed);
+
+        while(opModeIsActive() && hardware.arm.isBusy()) {
+            telemetry.addData("Arm encoder", hardware.arm.getCurrentPosition());
+            telemetry.update();
+        }
+
+        hardware.arm.setPower(0.0);
+
+        hardware.arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+    }
+
+    public void driveInches(double inches, double speed) {
+        driveEncoderCounts((int) (inches * hardware.COUNTS_PER_INCH), speed);
+
+    }
+
+    public void driveEncoderCounts(int counts, double speed) {
+        hardware.frontLeft.setTargetPosition    (hardware.frontLeft.getCurrentPosition() - counts);
+        hardware.frontRight.setTargetPosition   (hardware.frontRight.getCurrentPosition() - counts);
+        hardware.rearLeft.setTargetPosition     (hardware.rearLeft.getCurrentPosition() - counts);
+        hardware.rearRight.setTargetPosition    (hardware.rearRight.getCurrentPosition() - counts);
+
+        hardware.frontLeft.setMode  (DcMotor.RunMode.RUN_TO_POSITION);
+        hardware.frontRight.setMode (DcMotor.RunMode.RUN_TO_POSITION);
+        hardware.rearLeft.setMode   (DcMotor.RunMode.RUN_TO_POSITION);
+        hardware.rearRight.setMode  (DcMotor.RunMode.RUN_TO_POSITION);
+
+        hardware.setLeftPower(speed);
+        hardware.setRightPower(speed);
+
+        while(opModeIsActive() &&
+                hardware.frontLeft.isBusy() &&
+                hardware.frontRight.isBusy() &&
+                hardware.rearLeft.isBusy() &&
+                hardware.rearRight.isBusy()) {
+            telemetry.addData("Front left encoder", hardware.frontLeft.getCurrentPosition());
+            telemetry.addData("Front right encoder", hardware.frontRight.getCurrentPosition());
+            telemetry.update();
+        }
+
+        hardware.setLeftPower(0.0);
+        hardware.setRightPower(0.0);
+
+        hardware.frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        hardware.frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        hardware.rearLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        hardware.rearRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
     }
 }
