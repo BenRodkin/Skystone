76c729a80352806a824d00d0ca0981f0b4b245b4
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-10-25 18:38:25 -0400

    Added AutoBlueFoundation
    This program will pull the Foundation into the corner and park.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBlueFoundation.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name = "Blue Foundation", group = "Autonomous")
+public class AutoBlueFoundation extends LinearOpMode {
+
+    // Hardware class
+    SLICBotHardware hardware = new SLICBotHardware();
+
+
+    public void runOpMode() {
+
+        
+        waitForStart();
+
+        while(opModeIsActive()) {
+
+        }
+    }
+}
