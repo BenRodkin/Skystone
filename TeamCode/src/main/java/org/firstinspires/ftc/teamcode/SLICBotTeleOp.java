45bfc8eba928db564f3f8bd5ae85b51aedf8cb33
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-10-25 22:19:12 -0400

    Added holder and clamp telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SLICBotTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SLICBotTeleOp.java
@@ -96,6 +96,8 @@ public class SLICBotTeleOp extends OpMode {
 //        telemetry.addLine();
 //        telemetry.addData("Deploy1 position", hardware.deploy1.getPosition());
 //        telemetry.addData("Deploy2 position", hardware.deploy2.getPosition());
+        telemetry.addData("Holder position", hardware.holder.getPosition());
+        telemetry.addData("Clamp position", hardware.clamp.getPosition());
         telemetry.update();
     }
 
