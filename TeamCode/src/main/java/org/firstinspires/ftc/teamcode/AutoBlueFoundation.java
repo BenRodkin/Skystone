fe04d51f4c1ce85739462789273f0d03ca015e82
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-10-25 22:17:04 -0400

    Enabled setOutputRange() call in turnToHeadingPID()

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBlueFoundation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBlueFoundation.java
@@ -105,7 +105,7 @@ public class AutoBlueFoundation extends LinearOpMode {
         telemetry.addLine("Press dpad_down to stop.");
 
         hardware.pid.setSetpoint(target);                                       // Set target final heading relative to current
-        //hardware.pid.setOutputRange(-hardware.MAX_SPEED, hardware.MAX_SPEED);   // Set maximum motor power
+        hardware.pid.setOutputRange(-hardware.MAX_SPEED, hardware.MAX_SPEED);   // Set maximum motor power
         hardware.pid.setDeadband(hardware.TOLERANCE);                           // Set how far off you can safely be from your target
 
         while (opModeIsActive()) {
