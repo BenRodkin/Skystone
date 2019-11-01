f24599d4e961c97bd82443331413e23b83db6e1f
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-11-01 19:20:46 -0400

    Added arm and lift encoder telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestArm.java
@@ -6,13 +6,28 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp (name = "Test: Arm & Lift Presets", group = "Testing")
 public class TestArm extends OpMode {
+    SLICBotHardware hardware = new SLICBotHardware();
 
     @Override
     public void init() {
+        hardware.init(hardwareMap);
+
+        telemetry.addLine("Ready");
+        telemetry.update();
     }
 
 
     @Override
     public void loop() {
+
+
+        telemetry.addData("Arm Position", hardware.arm.getCurrentPosition());
+        telemetry.addData("Lift Position", hardware.pulley.getCurrentPosition());
+        telemetry.update();
+
+
     }
+
+    }
+
 }
