89528a581d5ead5a6f73dc9e61c5e63be865509a
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-11-01 19:19:09 -0400

    Created TestArm OpMode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestArm.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp (name = "Test: Arm & Lift Presets", group = "Testing")
+public class TestArm extends OpMode {
+
+    @Override
+    public void init() {
+    }
+
+
+    @Override
+    public void loop() {
+    }
+}
