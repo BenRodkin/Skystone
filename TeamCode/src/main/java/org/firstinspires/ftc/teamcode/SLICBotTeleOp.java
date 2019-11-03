5ec20ad2f127b0f77ad4e0aa6de89d61d9b11ab3
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-11-03 17:58:41 -0500

    Implemented arm and lift presets from TestArm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SLICBotTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SLICBotTeleOp.java
@@ -14,13 +14,44 @@ public class SLICBotTeleOp extends OpMode {
     // Button cooldowns
     ButtonCooldown gp2_a = new ButtonCooldown();
     ButtonCooldown gp2_x = new ButtonCooldown();
+    ButtonCooldown gp2_lb   = new ButtonCooldown();
+    ButtonCooldown gp2_rb   = new ButtonCooldown();
+    ButtonCooldown gp2_y    = new ButtonCooldown();
+
     public double runtime;
+
+
+    public final int ARM_STOWED         = 0;
+    public final int ARM_GRABBING       = -1200;
+    public final int ARM_PLACING_LOW    = -1200;
+    public final int ARM_PLACING_HIGH   = -900;
+
+    public final int LIFT_STOWED    = 0;
+    public final int LIFT_1         = 3468;
+    public final int LIFT_2         = 7951;
+    public final int LIFT_3         = 5879;
+    public final int LIFT_4         = 9642;
+
+    public int liftStep = -1;   // Default state (cannot be reached during driver control)
+    public int armStep  = -1;   // Default state (cannot be reached during driver control)
+
+
+    public final double ANALOG_THRESHOLD = 0.05;
 
     public void init() {
 
         hardware.init(hardwareMap);
         gp2_a.setCooldown(1.000);
         gp2_x.setCooldown(1.000);
+        gp2_lb  .setCooldown(1.000);
+        gp2_rb  .setCooldown(1.000);
+        gp2_y   .setCooldown(1.000);
+
+        hardware.arm.setTargetPosition(hardware.arm.getCurrentPosition()); // Default target
+        hardware.pulley.setTargetPosition(hardware.pulley.getCurrentPosition()); // Default target
+
+        hardware.arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        hardware.pulley.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
         telemetry.addLine("Ready");
         telemetry.update();
@@ -41,6 +72,29 @@ public class SLICBotTeleOp extends OpMode {
 
 
 
+        // Arm and lift position cycling
+        if(gamepad2.right_bumper && gp2_rb.ready(runtime) && liftStep < 4) {
+            liftStep ++;
+            gp2_rb.updateSnapshot(runtime);
+        }
+        if(gamepad2.left_bumper && gp2_lb.ready(runtime) && liftStep > 0) {
+            liftStep --;
+            gp2_lb.updateSnapshot(runtime);
+        }
+
+        if(gamepad2.y && gp2_y.ready(runtime)) {
+            armStep ++;
+            armStep %= 3;
+            gp2_y.updateSnapshot(runtime);
+        }
+
+        int armPlacingVal = ( liftStep <= 2 ? ARM_PLACING_LOW : ARM_PLACING_HIGH);
+        // End arm and lift position cycling
+
+
+
+
+
         double drive = -gamepad1.left_trigger + gamepad1.right_trigger;
         double turn = -gamepad1.left_stick_x * 3 / 4;
 
@@ -52,11 +106,69 @@ public class SLICBotTeleOp extends OpMode {
             hardware.setRightPower((drive + turn) * hardware.driverSpeedMod);
         }
 
-        double pulleySpeed = gamepad2.right_trigger - gamepad2.left_trigger;
 
-        hardware.pulley.setPower(pulleySpeed);
+        if(gamepad2.left_bumper || gamepad2.right_bumper) {
+            hardware.pulley.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            switch (liftStep) {
+                case -1:
+                    hardware.pulley.setTargetPosition(hardware.pulley.getCurrentPosition());
+                    break;
+                case 0:
+                    hardware.pulley.setTargetPosition(LIFT_STOWED);
+                    break;
+                case 1:
+                    hardware.pulley.setTargetPosition(LIFT_1);
+                    break;
+                case 2:
+                    hardware.pulley.setTargetPosition(LIFT_2);
+                    break;
+                case 3:
+                    hardware.pulley.setTargetPosition(LIFT_3);
+                    break;
+                case 4:
+                    hardware.pulley.setTargetPosition(LIFT_4);
+                    break;
+                default:
+                    break;
+            }
+            hardware.pulley.setPower(0.3);
+        } else if(Math.abs(gamepad2.right_trigger - gamepad2.left_trigger) > ANALOG_THRESHOLD){
+            hardware.pulley.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            hardware.pulley.setPower(gamepad2.right_trigger - gamepad2.left_trigger);
+            hardware.pulley.setTargetPosition(hardware.pulley.getCurrentPosition());
+        } else {
+            hardware.pulley.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            hardware.pulley.setPower(0.3);
+        }
 
-        hardware.arm.setPower(gamepad2.left_stick_y * 0.3);
+        if(gamepad2.y) {
+            hardware.arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            switch (armStep) {
+                case -1:
+                    hardware.arm.setTargetPosition(hardware.arm.getCurrentPosition());
+                    break;
+                case 0:
+                    hardware.arm.setTargetPosition(ARM_STOWED);
+                    break;
+                case 1:
+                    hardware.arm.setTargetPosition(ARM_GRABBING);
+                    break;
+                case 2:
+                    hardware.arm.setTargetPosition(armPlacingVal);
+                    break;
+                default:
+                    break;
+            }
+            hardware.arm.setPower(0.3);
+        } else if(Math.abs(gamepad2.left_stick_y) > ANALOG_THRESHOLD) {
+            hardware.arm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            hardware.arm.setPower(gamepad2.left_stick_y * 0.3);
+            hardware.arm.setTargetPosition(hardware.arm.getCurrentPosition());
+        } else {
+            hardware.arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            hardware.arm.setPower(0.3);
+        }
+
 
 
         if (gamepad2.a && gp2_a.ready(runtime)) {
