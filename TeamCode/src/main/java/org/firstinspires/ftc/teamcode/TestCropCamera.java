5bb064c11e5fc828712ebf2bbbd18a6b5537ccfd
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-11-10 15:21:16 -0500

    Copied in rectangle draw command from InternalCameraExample

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestCropCamera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestCropCamera.java
@@ -7,6 +7,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvCamera;
@@ -37,8 +38,6 @@ public class TestCropCamera extends LinearOpMode {
         while(opModeIsActive()) {
 
 
-
-
             telemetry.addLine("Running");
             telemetry.update();
         }
@@ -52,7 +51,18 @@ public class TestCropCamera extends LinearOpMode {
 
         @Override
         public Mat processFrame(Mat input) {
-                    return input;
+
+            Imgproc.rectangle(
+                    input,
+                    new Point(
+                            input.cols()/4,
+                            input.rows()/4),
+                    new Point(
+                            input.cols()*(3f/4f),
+                            input.rows()*(3f/4f)),
+                    new Scalar(0, 255, 0), 4);
+
+            return input;
         }
     }
 
