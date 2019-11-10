9366ed588ffa69a938fe0038c6cb0fc6aea4d11e
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-11-10 15:03:29 -0500

    Disabled TestArm OpMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestArm.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+@Disabled
 @TeleOp (name = "Test: Arm & Lift Presets", group = "Testing")
 public class TestArm extends OpMode {
     SLICBotHardware hardware = new SLICBotHardware();
