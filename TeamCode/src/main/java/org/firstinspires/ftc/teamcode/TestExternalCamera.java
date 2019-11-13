06d76825e02437ae878c56667c418c99033c066a
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-11-12 19:45:30 -0500

    Added TestCameraExample testing OpMode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestExternalCamera.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+@Autonomous(name = "Test: External Camera", group = "Testing")
+public class TestExternalCamera extends LinearOpMode {
+
+
+    public void runOpMode() {
+
+    }
+}
+
+
