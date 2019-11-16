d077f4a71decf6ac539bf30f0a51f73149c2fa81
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-11-15 21:23:30 -0500

    Added TestSkystonePipeline testing OpMode
    We will use this OpMode to develop a pipeline that can determine Systone randomization patterns.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestSkystonePipeline.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name = "Test: Skystone Pipeline", group = "Testing")
+public class TestSkystonePipeline extends LinearOpMode {
+
+
+
+    public void runOpMode() {
+
+
+        waitForStart();
+
+
+        while(opModeIsActive()) {
+            
+        }
+    }
+}
