f57f4af4acec89b83c7c9ff88e6b53b3893505f9
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-11-15 21:26:11 -0500

    Added phone camera reference and local pipeline

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestSkystonePipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestSkystonePipeline.java
@@ -3,9 +3,18 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvPipeline;
+
 @Autonomous(name = "Test: Skystone Pipeline", group = "Testing")
 public class TestSkystonePipeline extends LinearOpMode {
 
+    OpenCvCamera phoneCam;
+    SkystonePatternPipeline skystonePatternPipeline;
 
 
     public void runOpMode() {
@@ -15,7 +24,18 @@ public class TestSkystonePipeline extends LinearOpMode {
 
 
         while(opModeIsActive()) {
+
+        }
+    }
+
+    static class SkystonePatternPipeline extends OpenCvPipeline {
+
+        @Override
+        public Mat processFrame(Mat input) {
+
             
+
+            return input;
         }
     }
 }
