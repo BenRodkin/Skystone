71a1d781c04c992e33e3f4fa9de1791688418ffc
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-11-18 21:24:04 -0500

    Added experimental Mecanum drive controls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SlippyBotTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SlippyBotTeleOp.java
@@ -8,6 +8,11 @@ public class SlippyBotTeleOp extends OpMode {
 
     SlippyBotHardware hardware = new SlippyBotHardware();
 
+    double flPower = 0.0;
+    double frPower = 0.0;
+    double rlPower = 0.0;
+    double rrPower = 0.0;
+
     public void init() {
         hardware.init(hardwareMap);
 
@@ -17,6 +22,23 @@ public class SlippyBotTeleOp extends OpMode {
 
     public void loop() {
 
+        // Do the math
+        double drive  = gamepad1.left_stick_y;
+        double strafe = gamepad1.left_stick_x;
+        double twist  = gamepad1.right_stick_x;
+
+        flPower = (drive + strafe + twist);
+        frPower = (drive - strafe - twist);
+        rlPower = (drive - strafe + twist);
+        rrPower = (drive + strafe - twist);
+
+
+
+        // Set the power
+        hardware.frontLeft. setPower(flPower);
+        hardware.frontRight.setPower(frPower);
+        hardware.rearLeft.  setPower(rlPower);
+        hardware.rearRight. setPower(rrPower);
 
 
         telemetry.addLine("Running");
