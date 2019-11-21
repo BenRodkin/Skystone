e97affb7b320b9adbb844580670ab3987a82887d
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-11-21 18:54:39 -0500

    Added skystone placement variable controls
    These controls will let us pick which program to test.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestQuarryPaths.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestQuarryPaths.java
@@ -3,8 +3,14 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import static org.firstinspires.ftc.teamcode.SkystonePlacement.CENTER;
+import static org.firstinspires.ftc.teamcode.SkystonePlacement.LEFT;
+import static org.firstinspires.ftc.teamcode.SkystonePlacement.RIGHT;
+
 @Autonomous(name = "Test: Quarry-Side Paths", group = "Testing")
 public class TestQuarryPaths extends LinearOpMode {
+
+    SkystonePlacement placement = CENTER; // Default is center
 
 
 
@@ -18,7 +24,16 @@ public class TestQuarryPaths extends LinearOpMode {
 
         while(opModeIsActive()) {
 
+            if(gamepad1.dpad_left)  placement   = LEFT;
+            if(gamepad1.dpad_up)    placement   = CENTER;
+            if(gamepad1.dpad_right) placement   = RIGHT;
+
+
+
+
             telemetry.addLine("Running");
+            telemetry.addLine();
+            telemetry.addData("Skystone Placement", placement);
             telemetry.update();
         }
 
