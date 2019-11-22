ac26a2ea0762f731e15c86926ef0263c3c5f7f70
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-11-21 21:22:17 -0500

    Added arm deployment step to paths

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestQuarryPaths.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestQuarryPaths.java
@@ -88,6 +88,11 @@ public class TestQuarryPaths extends LinearOpMode {
         hardware.pid.setOutputRange(-(hardware.MAX_SPEED / 2.0), (hardware.MAX_SPEED / 2.0));
         turnToHeadingPID(15);
         hardware.pid.setOutputRange(-hardware.MAX_SPEED, hardware.MAX_SPEED);
+
+        // Lower arm
+        moveArmCounts(hardware.ARM_COUNTS_DEPLOY, 0.3);
+
+
     }
 
     public void runCenter() throws InterruptedException {
@@ -102,6 +107,11 @@ public class TestQuarryPaths extends LinearOpMode {
         hardware.pid.setOutputRange(-(hardware.MAX_SPEED / 2.0), (hardware.MAX_SPEED / 2.0));
         turnToHeadingPID(0);
         hardware.pid.setOutputRange(-hardware.MAX_SPEED, hardware.MAX_SPEED);
+
+        // Lower arm
+        moveArmCounts(hardware.ARM_COUNTS_DEPLOY, 0.3);
+
+
     }
 
     public void runRight() throws InterruptedException {
@@ -116,6 +126,11 @@ public class TestQuarryPaths extends LinearOpMode {
         hardware.pid.setOutputRange(-(hardware.MAX_SPEED / 2.0), (hardware.MAX_SPEED / 2.0));
         turnToHeadingPID(-15);
         hardware.pid.setOutputRange(-hardware.MAX_SPEED, hardware.MAX_SPEED);
+
+        // Lower arm
+        moveArmCounts(hardware.ARM_COUNTS_DEPLOY, 0.3);
+
+
     }
 
 
