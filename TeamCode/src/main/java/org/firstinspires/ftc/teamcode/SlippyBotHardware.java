9062228d905cc09485f36a1788accd1ff6243850
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-11-27 14:11:38 -0500

    Added PID variables

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SlippyBotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SlippyBotHardware.java
@@ -13,6 +13,15 @@ public class SlippyBotHardware {
 
     public DcMotor pulley;
 
+    // PID variables
+    public final double MAX_SPEED = 0.4;
+    public final double P = 0.02;
+    public final double I = 0.0;
+    public final double D = 0.1;
+    public final double TOLERANCE = 1;
+
+    public final SynchronousPID pid = new SynchronousPID(P, I, D);
+
 
     public void init(HardwareMap hardwareMap) {
         frontLeft   = hardwareMap.dcMotor.get("fl_drive");
