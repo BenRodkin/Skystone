8b0f5625de8f9ece49d62de3035b9e6974c97c46
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-11-27 14:03:51 -0500

    Added pulley motor to Mecanum bot hardware class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SlippyBotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SlippyBotHardware.java
@@ -11,12 +11,16 @@ public class SlippyBotHardware {
     public DcMotor rearLeft;
     public DcMotor rearRight;
 
+    public DcMotor pulley;
+
 
     public void init(HardwareMap hardwareMap) {
         frontLeft   = hardwareMap.dcMotor.get("fl_drive");
         frontRight  = hardwareMap.dcMotor.get("fr_drive");
         rearLeft    = hardwareMap.dcMotor.get("rl_drive");
         rearRight   = hardwareMap.dcMotor.get("rr_drive");
+
+        pulley      = hardwareMap.dcMotor.get("pulley");
 
 
         frontLeft.  setDirection(DcMotorSimple.Direction.REVERSE);
@@ -26,6 +30,8 @@ public class SlippyBotHardware {
         frontRight. setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rearLeft.   setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rearRight.  setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        pulley.     setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
 }
