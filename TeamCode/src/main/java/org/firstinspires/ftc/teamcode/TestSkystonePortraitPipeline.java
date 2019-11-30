a22a59181b94641ea8a222740bf65705fb7c8eed
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-11-30 10:35:19 -0500

    Deactivated local pipeline class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestSkystonePortraitPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestSkystonePortraitPipeline.java
@@ -320,10 +320,6 @@ public class TestSkystonePortraitPipeline extends LinearOpMode {
                         compareAreaTallies(contoursProportionLeft, contoursProportionCenter, contoursProportionRight);
             }
 
-
-
-
-
             telemetry.addLine("Running");
             telemetry.addLine(String.format("Hue: [%s, %s]", hsvHue[0], hsvHue[1]));
             telemetry.addLine(String.format("Sat: [%s, %s]", hsvSat[0], hsvSat[1]));
@@ -347,7 +343,6 @@ public class TestSkystonePortraitPipeline extends LinearOpMode {
         return input;
     }
 
-
     public double largest(double tallyLeft, double tallyCenter, double tallyRight) {
         return Math.max(Math.max(tallyLeft, tallyCenter), tallyRight);
     }
@@ -355,9 +350,6 @@ public class TestSkystonePortraitPipeline extends LinearOpMode {
     public double smallest(double tallyLeft, double tallyCenter, double tallyRight) {
         return Math.min(Math.min(tallyLeft, tallyCenter), tallyRight);
     }
-
-
-
 
     public SkystonePlacement compareAreaTallies(double tallyLeft, double tallyCenter, double tallyRight) {
         // Tally counts the area contained by yellow blobs in each third of the screen
@@ -371,9 +363,6 @@ public class TestSkystonePortraitPipeline extends LinearOpMode {
         return CENTER;                                      // Default case
     }
 
-
-
-
     /**
      * SkystonePatternPipeline class.
      *
@@ -381,192 +370,192 @@ public class TestSkystonePortraitPipeline extends LinearOpMode {
      *
      * @author GRIP
      */
-    private static class SkystonePatternPipeline extends OpenCvPipeline {
-
-
-        //Outputs
-        private Mat hsvThresholdOutput = new Mat();
-        private ArrayList<MatOfPoint> findContoursOutput = new ArrayList<MatOfPoint>();
-        private ArrayList<MatOfPoint> filterContoursOutput = new ArrayList<MatOfPoint>();
-
-        @Override
-        public Mat processFrame(Mat input) {
-
-            // Step HSV_Threshold0:
-            Mat hsvThresholdInput = input;
-
-            if(drawRect) {
-                Imgproc.rectangle(
-                        input,
-                        new Point(  // Top left corner
-                                rectLeft,   // Left value
-                                rectTop),   // Top value
-                        new Point( // Bottom right corner
-                                rectRight,  // Right value
-                                rectBot),   // Bottom value
-                        new Scalar(0, 255, 0), 4);
-            }
-            Imgproc.line(
-                    input,
-                    new Point(
-                            leftBound,
-                            rectBot),
-                    new Point(
-                            leftBound,
-                            rectTop),
-                    new Scalar(0, 0, 255), 4);
-            Imgproc.line(
-                    input,
-                    new Point(
-                            centerBound,
-                            rectBot),
-                    new Point(
-                            centerBound,
-                            rectTop),
-                    new Scalar(255, 0, 0), 4);
-
-            double[] hsvThresholdHue =          hsvHue;
-            double[] hsvThresholdSaturation =   hsvSat;
-            double[] hsvThresholdValue =        hsvVal;
-            hsvThreshold(hsvThresholdInput, hsvThresholdHue, hsvThresholdSaturation, hsvThresholdValue, hsvThresholdOutput);
-
-
-            // Step Find_Contours0:
-            Mat findContoursInput = hsvThresholdOutput;
-            boolean findContoursExternalOnly = false;
-            findContours(findContoursInput, findContoursExternalOnly, findContoursOutput);
-
-            // Step Filter_Contours0:
-            ArrayList<MatOfPoint> filterContoursContours = findContoursOutput;
-            double filterContoursMinArea = 100.0;
-            double filterContoursMinPerimeter = 0.0;
-            double filterContoursMinWidth = 0.0;
-            double filterContoursMaxWidth = 2.147483647E9;
-            double filterContoursMinHeight = 0.0;
-            double filterContoursMaxHeight = 2.147483647E9;
-            double[] filterContoursSolidity = {0, 100};
-            double filterContoursMaxVertices = 2.147483647E9;
-            double filterContoursMinVertices = 0.0;
-            double filterContoursMinRatio = 0.0;
-            double filterContoursMaxRatio = 2.147483647E9;
-            filterContours(filterContoursContours, filterContoursMinArea, filterContoursMinPerimeter, filterContoursMinWidth, filterContoursMaxWidth, filterContoursMinHeight, filterContoursMaxHeight, filterContoursSolidity, filterContoursMaxVertices, filterContoursMinVertices, filterContoursMinRatio, filterContoursMaxRatio, filterContoursOutput);
-
-
-
-            return (returnHSV ? hsvThresholdOutput : input);
-        }
-
-
-        /**
-         * This method is a generated getter for the output of a HSV_Threshold.
-         * @return Mat output from HSV_Threshold.
-         */
-        public Mat hsvThresholdOutput() {
-            return hsvThresholdOutput;
-        }
-
-        /**
-         * This method is a generated getter for the output of a Find_Contours.
-         * @return ArrayList<MatOfPoint> output from Find_Contours.
-         */
-        public ArrayList<MatOfPoint> findContoursOutput() {
-            return findContoursOutput;
-        }
-
-        /**
-         * This method is a generated getter for the output of a Filter_Contours.
-         * @return ArrayList<MatOfPoint> output from Filter_Contours.
-         */
-        public ArrayList<MatOfPoint> filterContoursOutput() {
-            return filterContoursOutput;
-        }
-
-
-        /**
-         * Segment an image based on hue, saturation, and value ranges.
-         *
-         * @param input The image on which to perform the HSL threshold.
-         * @param hue The min and max hue
-         * @param sat The min and max saturation
-         * @param val The min and max value
-         * @param output The image in which to store the output.
-         */
-        private void hsvThreshold(Mat input, double[] hue, double[] sat, double[] val,
-                                  Mat out) {
-            Imgproc.cvtColor(input, out, Imgproc.COLOR_BGR2HSV);
-            Core.inRange(out, new Scalar(hue[0], sat[0], val[0]),
-                    new Scalar(hue[1], sat[1], val[1]), out);
-        }
-
-        /**
-         * Sets the values of pixels in a binary image to their distance to the nearest black pixel.
-         * @param input The image on which to perform the Distance Transform.
-         * @param type The Transform.
-         * @param maskSize the size of the mask.
-         * @param output The image in which to store the output.
-         */
-        private void findContours(Mat input, boolean externalOnly,
-                                  List<MatOfPoint> contours) {
-            Mat hierarchy = new Mat();
-            contours.clear();
-            int mode;
-            if (externalOnly) {
-                mode = Imgproc.RETR_EXTERNAL;
-            }
-            else {
-                mode = Imgproc.RETR_LIST;
-            }
-            int method = Imgproc.CHAIN_APPROX_SIMPLE;
-            Imgproc.findContours(input, contours, hierarchy, mode, method);
-        }
-
-
-        /**
-         * Filters out contours that do not meet certain criteria.
-         * @param inputContours is the input list of contours
-         * @param output is the the output list of contours
-         * @param minArea is the minimum area of a contour that will be kept
-         * @param minPerimeter is the minimum perimeter of a contour that will be kept
-         * @param minWidth minimum width of a contour
-         * @param maxWidth maximum width
-         * @param minHeight minimum height
-         * @param maxHeight maximimum height
-         * @param Solidity the minimum and maximum solidity of a contour
-         * @param minVertexCount minimum vertex Count of the contours
-         * @param maxVertexCount maximum vertex Count
-         * @param minRatio minimum ratio of width to height
-         * @param maxRatio maximum ratio of width to height
-         */
-        private void filterContours(List<MatOfPoint> inputContours, double minArea,
-                                    double minPerimeter, double minWidth, double maxWidth, double minHeight, double
-                                            maxHeight, double[] solidity, double maxVertexCount, double minVertexCount, double
-                                            minRatio, double maxRatio, List<MatOfPoint> output) {
-            final MatOfInt hull = new MatOfInt();
-            output.clear();
-            //operation
-            for (int i = 0; i < inputContours.size(); i++) {
-                final MatOfPoint contour = inputContours.get(i);
-                final Rect bb = Imgproc.boundingRect(contour);
-                if (bb.width < minWidth || bb.width > maxWidth) continue;
-                if (bb.height < minHeight || bb.height > maxHeight) continue;
-                final double area = Imgproc.contourArea(contour);
-                if (area < minArea) continue;
-                if (Imgproc.arcLength(new MatOfPoint2f(contour.toArray()), true) < minPerimeter) continue;
-                Imgproc.convexHull(contour, hull);
-                MatOfPoint mopHull = new MatOfPoint();
-                mopHull.create((int) hull.size().height, 1, CvType.CV_32SC2);
-                for (int j = 0; j < hull.size().height; j++) {
-                    int index = (int)hull.get(j, 0)[0];
-                    double[] point = new double[] { contour.get(index, 0)[0], contour.get(index, 0)[1]};
-                    mopHull.put(j, 0, point);
-                }
-                final double solid = 100 * area / Imgproc.contourArea(mopHull);
-                if (solid < solidity[0] || solid > solidity[1]) continue;
-                if (contour.rows() < minVertexCount || contour.rows() > maxVertexCount)	continue;
-                final double ratio = bb.width / (double)bb.height;
-                if (ratio < minRatio || ratio > maxRatio) continue;
-                output.add(contour);
-            }
-        }
-    }
+//    private static class SkystonePatternPipeline extends OpenCvPipeline {
+//
+//
+//        //Outputs
+//        private Mat hsvThresholdOutput = new Mat();
+//        private ArrayList<MatOfPoint> findContoursOutput = new ArrayList<MatOfPoint>();
+//        private ArrayList<MatOfPoint> filterContoursOutput = new ArrayList<MatOfPoint>();
+//
+//        @Override
+//        public Mat processFrame(Mat input) {
+//
+//            // Step HSV_Threshold0:
+//            Mat hsvThresholdInput = input;
+//
+//            if(drawRect) {
+//                Imgproc.rectangle(
+//                        input,
+//                        new Point(  // Top left corner
+//                                rectLeft,   // Left value
+//                                rectTop),   // Top value
+//                        new Point( // Bottom right corner
+//                                rectRight,  // Right value
+//                                rectBot),   // Bottom value
+//                        new Scalar(0, 255, 0), 4);
+//            }
+//            Imgproc.line(
+//                    input,
+//                    new Point(
+//                            leftBound,
+//                            rectBot),
+//                    new Point(
+//                            leftBound,
+//                            rectTop),
+//                    new Scalar(0, 0, 255), 4);
+//            Imgproc.line(
+//                    input,
+//                    new Point(
+//                            centerBound,
+//                            rectBot),
+//                    new Point(
+//                            centerBound,
+//                            rectTop),
+//                    new Scalar(255, 0, 0), 4);
+//
+//            double[] hsvThresholdHue =          hsvHue;
+//            double[] hsvThresholdSaturation =   hsvSat;
+//            double[] hsvThresholdValue =        hsvVal;
+//            hsvThreshold(hsvThresholdInput, hsvThresholdHue, hsvThresholdSaturation, hsvThresholdValue, hsvThresholdOutput);
+//
+//
+//            // Step Find_Contours0:
+//            Mat findContoursInput = hsvThresholdOutput;
+//            boolean findContoursExternalOnly = false;
+//            findContours(findContoursInput, findContoursExternalOnly, findContoursOutput);
+//
+//            // Step Filter_Contours0:
+//            ArrayList<MatOfPoint> filterContoursContours = findContoursOutput;
+//            double filterContoursMinArea = 100.0;
+//            double filterContoursMinPerimeter = 0.0;
+//            double filterContoursMinWidth = 0.0;
+//            double filterContoursMaxWidth = 2.147483647E9;
+//            double filterContoursMinHeight = 0.0;
+//            double filterContoursMaxHeight = 2.147483647E9;
+//            double[] filterContoursSolidity = {0, 100};
+//            double filterContoursMaxVertices = 2.147483647E9;
+//            double filterContoursMinVertices = 0.0;
+//            double filterContoursMinRatio = 0.0;
+//            double filterContoursMaxRatio = 2.147483647E9;
+//            filterContours(filterContoursContours, filterContoursMinArea, filterContoursMinPerimeter, filterContoursMinWidth, filterContoursMaxWidth, filterContoursMinHeight, filterContoursMaxHeight, filterContoursSolidity, filterContoursMaxVertices, filterContoursMinVertices, filterContoursMinRatio, filterContoursMaxRatio, filterContoursOutput);
+//
+//
+//
+//            return (returnHSV ? hsvThresholdOutput : input);
+//        }
+//
+//
+//        /**
+//         * This method is a generated getter for the output of a HSV_Threshold.
+//         * @return Mat output from HSV_Threshold.
+//         */
+//        public Mat hsvThresholdOutput() {
+//            return hsvThresholdOutput;
+//        }
+//
+//        /**
+//         * This method is a generated getter for the output of a Find_Contours.
+//         * @return ArrayList<MatOfPoint> output from Find_Contours.
+//         */
+//        public ArrayList<MatOfPoint> findContoursOutput() {
+//            return findContoursOutput;
+//        }
+//
+//        /**
+//         * This method is a generated getter for the output of a Filter_Contours.
+//         * @return ArrayList<MatOfPoint> output from Filter_Contours.
+//         */
+//        public ArrayList<MatOfPoint> filterContoursOutput() {
+//            return filterContoursOutput;
+//        }
+//
+//
+//        /**
+//         * Segment an image based on hue, saturation, and value ranges.
+//         *
+//         * @param input The image on which to perform the HSL threshold.
+//         * @param hue The min and max hue
+//         * @param sat The min and max saturation
+//         * @param val The min and max value
+//         * @param output The image in which to store the output.
+//         */
+//        private void hsvThreshold(Mat input, double[] hue, double[] sat, double[] val,
+//                                  Mat out) {
+//            Imgproc.cvtColor(input, out, Imgproc.COLOR_BGR2HSV);
+//            Core.inRange(out, new Scalar(hue[0], sat[0], val[0]),
+//                    new Scalar(hue[1], sat[1], val[1]), out);
+//        }
+//
+//        /**
+//         * Sets the values of pixels in a binary image to their distance to the nearest black pixel.
+//         * @param input The image on which to perform the Distance Transform.
+//         * @param type The Transform.
+//         * @param maskSize the size of the mask.
+//         * @param output The image in which to store the output.
+//         */
+//        private void findContours(Mat input, boolean externalOnly,
+//                                  List<MatOfPoint> contours) {
+//            Mat hierarchy = new Mat();
+//            contours.clear();
+//            int mode;
+//            if (externalOnly) {
+//                mode = Imgproc.RETR_EXTERNAL;
+//            }
+//            else {
+//                mode = Imgproc.RETR_LIST;
+//            }
+//            int method = Imgproc.CHAIN_APPROX_SIMPLE;
+//            Imgproc.findContours(input, contours, hierarchy, mode, method);
+//        }
+//
+//
+//        /**
+//         * Filters out contours that do not meet certain criteria.
+//         * @param inputContours is the input list of contours
+//         * @param output is the the output list of contours
+//         * @param minArea is the minimum area of a contour that will be kept
+//         * @param minPerimeter is the minimum perimeter of a contour that will be kept
+//         * @param minWidth minimum width of a contour
+//         * @param maxWidth maximum width
+//         * @param minHeight minimum height
+//         * @param maxHeight maximimum height
+//         * @param Solidity the minimum and maximum solidity of a contour
+//         * @param minVertexCount minimum vertex Count of the contours
+//         * @param maxVertexCount maximum vertex Count
+//         * @param minRatio minimum ratio of width to height
+//         * @param maxRatio maximum ratio of width to height
+//         */
+//        private void filterContours(List<MatOfPoint> inputContours, double minArea,
+//                                    double minPerimeter, double minWidth, double maxWidth, double minHeight, double
+//                                            maxHeight, double[] solidity, double maxVertexCount, double minVertexCount, double
+//                                            minRatio, double maxRatio, List<MatOfPoint> output) {
+//            final MatOfInt hull = new MatOfInt();
+//            output.clear();
+//            //operation
+//            for (int i = 0; i < inputContours.size(); i++) {
+//                final MatOfPoint contour = inputContours.get(i);
+//                final Rect bb = Imgproc.boundingRect(contour);
+//                if (bb.width < minWidth || bb.width > maxWidth) continue;
+//                if (bb.height < minHeight || bb.height > maxHeight) continue;
+//                final double area = Imgproc.contourArea(contour);
+//                if (area < minArea) continue;
+//                if (Imgproc.arcLength(new MatOfPoint2f(contour.toArray()), true) < minPerimeter) continue;
+//                Imgproc.convexHull(contour, hull);
+//                MatOfPoint mopHull = new MatOfPoint();
+//                mopHull.create((int) hull.size().height, 1, CvType.CV_32SC2);
+//                for (int j = 0; j < hull.size().height; j++) {
+//                    int index = (int)hull.get(j, 0)[0];
+//                    double[] point = new double[] { contour.get(index, 0)[0], contour.get(index, 0)[1]};
+//                    mopHull.put(j, 0, point);
+//                }
+//                final double solid = 100 * area / Imgproc.contourArea(mopHull);
+//                if (solid < solidity[0] || solid > solidity[1]) continue;
+//                if (contour.rows() < minVertexCount || contour.rows() > maxVertexCount)	continue;
+//                final double ratio = bb.width / (double)bb.height;
+//                if (ratio < minRatio || ratio > maxRatio) continue;
+//                output.add(contour);
+//            }
+//        }
+//    }
 }
