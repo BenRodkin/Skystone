da6e3983bf560bd05c717be376b23c51e6e2a514
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-11-30 10:49:06 -0500

    Cleaned up import statements

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestSkystonePortraitPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestSkystonePortraitPipeline.java
@@ -4,22 +4,14 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.Range;
 
-import org.opencv.core.Core;
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
-import org.opencv.core.MatOfInt;
 import org.opencv.core.MatOfPoint;
-import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
-import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvInternalCamera;
-import org.openftc.easyopencv.OpenCvPipeline;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
