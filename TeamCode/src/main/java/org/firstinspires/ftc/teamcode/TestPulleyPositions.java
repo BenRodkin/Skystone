54012c08652c0b839f9eac44e6d63f67e0f14cb5
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-12-01 14:35:04 -0500

    Added pulley controls and telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestPulleyPositions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestPulleyPositions.java
@@ -5,13 +5,28 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp(name = "Test: Pulley Positions", group = "Testing")
 public class TestPulleyPositions extends OpMode {
+    SlippyBotHardware hardware = new SlippyBotHardware();
+
 
 
     @Override
     public void init() {
+        hardware.init(hardwareMap);
+
+        telemetry.addLine("Ready");
+        telemetry.update();
     }
 
     @Override
     public void loop() {
+        
+        hardware.pulleyLeft.setPower(gamepad1.left_stick_y * 0.8);
+        hardware.pulleyRight.setPower(gamepad1.right_stick_y * 0.8);
+
+        telemetry.addLine("Running");
+        telemetry.addLine();
+        telemetry.addData("Left Pulley Position", hardware.pulleyLeft.getCurrentPosition());
+        telemetry.addData("Right Pulley Position", hardware.pulleyRight.getCurrentPosition());
+        telemetry.update();
     }
 }
