dcea9f2f733684fe50694b290f114c0f58e91656
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-12-01 14:34:16 -0500

    Created TestPulleyPositions testing OpMode
    Used to control the pulley motors independently and ensure that the encoders work as expected.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestPulleyPositions.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Test: Pulley Positions", group = "Testing")
+public class TestPulleyPositions extends OpMode {
+
+
+    @Override
+    public void init() {
+    }
+
+    @Override
+    public void loop() {
+    }
+}
