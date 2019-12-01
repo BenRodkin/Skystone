d0a4a884f2c63f1f9d08329c3e153eebe3c0e972
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-12-01 14:40:56 -0500

    Added controls to reset pulley encoders

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestPulleyPositions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestPulleyPositions.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp(name = "Test: Pulley Positions", group = "Testing")
 public class TestPulleyPositions extends OpMode {
@@ -19,14 +20,25 @@ public class TestPulleyPositions extends OpMode {
 
     @Override
     public void loop() {
-        
+
         hardware.pulleyLeft.setPower(gamepad1.left_stick_y * 0.8);
         hardware.pulleyRight.setPower(gamepad1.right_stick_y * 0.8);
+
+        if(gamepad1.b)
+            resetPulleyEncoders(hardware.pulleyLeft.getMode(), hardware.pulleyRight.getMode());
 
         telemetry.addLine("Running");
         telemetry.addLine();
         telemetry.addData("Left Pulley Position", hardware.pulleyLeft.getCurrentPosition());
         telemetry.addData("Right Pulley Position", hardware.pulleyRight.getCurrentPosition());
         telemetry.update();
+    }
+
+    public void resetPulleyEncoders(DcMotor.RunMode runModeLeft, DcMotor.RunMode runModeRight) {
+        hardware.pulleyLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        hardware.pulleyRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        hardware.pulleyLeft.setMode(runModeLeft);
+        hardware.pulleyRight.setMode(runModeRight);
     }
 }
