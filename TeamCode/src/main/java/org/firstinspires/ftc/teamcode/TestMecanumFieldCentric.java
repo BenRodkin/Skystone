9dc02743fbe3df2fbf215541c5d962d133bfbf1b
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-12-01 16:08:45 -0500

    Added hardware and IMU

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestMecanumFieldCentric.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestMecanumFieldCentric.java
@@ -1,13 +1,40 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 
 @TeleOp(name = "Test: Mecanum Drive, Field Centric", group = "Testing")
 public class TestMecanumFieldCentric extends OpMode {
 
+    // IMU
+    BNO055IMU imu;
+
+    // Robot hardware
+    SlippyBotHardware hardware = new SlippyBotHardware();
+
     @Override
     public void init() {
+        // Initialize hardware
+        hardware.init(hardwareMap);
+
+        // Initialize IMU
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
 
     }
 
