d3a47e82f63fbf6b87e0ff91b56bc2cb19eaf327
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-12-01 16:02:19 -0500

    Created TestMecanumFieldCentric testing OpMode
    This OpMode will be used to test our code for field-centric driver control of the robot.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestMecanumFieldCentric.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Test: Mecanum Drive, Field Centric", group = "Testing")
+public class TestMecanumFieldCentric extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+    
+}
