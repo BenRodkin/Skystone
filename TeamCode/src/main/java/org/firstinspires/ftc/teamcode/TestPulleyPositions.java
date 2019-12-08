a67ade5f036103d040d585951530fe19af182107
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-12-08 17:20:07 -0500

    Disabled slippy arm and pulley position test OpModes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestPulleyPositions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestPulleyPositions.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+@Disabled
 @TeleOp(name = "Test: Pulley Positions", group = "Testing")
 public class TestPulleyPositions extends OpMode {
     SlippyBotHardware hardware = new SlippyBotHardware();
