8c898655477a4bd53e723c66200e84f60e75ba24
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-12-11 17:25:42 -0500

    Created TestSlippyMovement testing OpMode
    We will use this OpMode to test our driveEncoderCounts(), strafeEncoderCounts(), driveInches(), and strafeInches() methods to be used for autonomous movement.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestSlippyMovement.java
@@ -0,0 +1,112 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous(name = "Test: Slippy Movement", group = "Testing")
+public class TestSlippyMovement extends LinearOpMode {
+
+    SlippyBotHardware hardware = new SlippyBotHardware();
+
+    private final double DRIVE_SPEED = 0.4;
+
+
+    @Override
+    public void runOpMode() {
+        hardware.init(hardwareMap);
+
+        telemetry.addLine("Ready");
+        telemetry.update();
+
+        waitForStart();
+
+        while(opModeIsActive()) {
+
+            // Movement testing controls
+            // Convention: forward and right (from the robot's perspective) are positive movement
+            if(gamepad1.dpad_up)    driveEncoderCounts(1000, DRIVE_SPEED);   // Drive forward
+            if(gamepad1.dpad_right) strafeEncoderCounts(1000, DRIVE_SPEED);  // Strafe right
+            if(gamepad1.dpad_down)  driveEncoderCounts(-1000, DRIVE_SPEED);  // Drive backward
+            if(gamepad1.dpad_left)  strafeEncoderCounts(-1000, DRIVE_SPEED); // Strafe left
+
+
+            telemetry.addData("Front left position",    hardware.frontLeft.getCurrentPosition());
+            telemetry.addData("Front right position",   hardware.frontRight.getCurrentPosition());
+            telemetry.addData("Rear left position",     hardware.rearLeft.getCurrentPosition());
+            telemetry.addData("Rear right position",    hardware.rearRight.getCurrentPosition());
+            telemetry.update();
+        }
+    }
+
+    private void driveEncoderCounts(int counts, double speed) {
+        hardware.frontLeft.setTargetPosition    (hardware.frontLeft.getCurrentPosition()    + counts);
+        hardware.frontRight.setTargetPosition   (hardware.frontRight.getCurrentPosition()   + counts);
+        hardware.rearLeft.setTargetPosition     (hardware.rearLeft.getCurrentPosition()     + counts);
+        hardware.rearRight.setTargetPosition    (hardware.rearRight.getCurrentPosition()    + counts);
+
+        hardware.frontLeft.setMode  (DcMotor.RunMode.RUN_TO_POSITION);
+        hardware.frontRight.setMode (DcMotor.RunMode.RUN_TO_POSITION);
+        hardware.rearLeft.setMode   (DcMotor.RunMode.RUN_TO_POSITION);
+        hardware.rearRight.setMode  (DcMotor.RunMode.RUN_TO_POSITION);
+
+        hardware.setLeftPower(speed);
+        hardware.setRightPower(speed);
+
+        while(opModeIsActive() &&
+                hardware.frontLeft.isBusy() &&
+                hardware.frontRight.isBusy() &&
+                hardware.rearLeft.isBusy() &&
+                hardware.rearRight.isBusy()) {
+            telemetry.addData("Front left encoder",     hardware.frontLeft.getCurrentPosition());
+            telemetry.addData("Front right encoder",    hardware.frontRight.getCurrentPosition());
+            telemetry.addData("Rear left encoder",      hardware.rearLeft.getCurrentPosition());
+            telemetry.addData("Rear right encoder",     hardware.rearRight.getCurrentPosition());
+            telemetry.update();
+        }
+
+        hardware.setLeftPower(0.0);
+        hardware.setRightPower(0.0);
+
+        hardware.frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        hardware.frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        hardware.rearLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        hardware.rearRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+    }
+
+    private void strafeEncoderCounts(int counts, double speed) {
+        hardware.frontLeft.setTargetPosition    (hardware.frontLeft.getCurrentPosition()    + counts);
+        hardware.frontRight.setTargetPosition   (hardware.frontRight.getCurrentPosition()   - counts);
+        hardware.rearLeft.setTargetPosition     (hardware.rearLeft.getCurrentPosition()     - counts);
+        hardware.rearRight.setTargetPosition    (hardware.rearRight.getCurrentPosition()    + counts);
+
+        hardware.frontLeft.setMode  (DcMotor.RunMode.RUN_TO_POSITION);
+        hardware.frontRight.setMode (DcMotor.RunMode.RUN_TO_POSITION);
+        hardware.rearLeft.setMode   (DcMotor.RunMode.RUN_TO_POSITION);
+        hardware.rearRight.setMode  (DcMotor.RunMode.RUN_TO_POSITION);
+
+        hardware.setLeftPower(speed);
+        hardware.setRightPower(speed);
+
+        while(opModeIsActive() &&
+                hardware.frontLeft.isBusy() &&
+                hardware.frontRight.isBusy() &&
+                hardware.rearLeft.isBusy() &&
+                hardware.rearRight.isBusy()) {
+            telemetry.addData("Front left encoder",     hardware.frontLeft.getCurrentPosition());
+            telemetry.addData("Front right encoder",    hardware.frontRight.getCurrentPosition());
+            telemetry.addData("Rear left encoder",      hardware.rearLeft.getCurrentPosition());
+            telemetry.addData("Rear right encoder",     hardware.rearRight.getCurrentPosition());
+            telemetry.update();
+        }
+
+        hardware.setLeftPower(0.0);
+        hardware.setRightPower(0.0);
+
+        hardware.frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        hardware.frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        hardware.rearLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        hardware.rearRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+}
