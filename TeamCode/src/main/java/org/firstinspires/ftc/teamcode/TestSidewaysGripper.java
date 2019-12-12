5431b984b51a8a6b3f06000c29941ccddf601f2c
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-12-12 13:59:47 -0500

    Scaled gripper range to match range for Actuonix PQ12 linear actuators

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestSidewaysGripper.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestSidewaysGripper.java
@@ -24,6 +24,17 @@ public class TestSidewaysGripper extends LinearOpMode {
         gripper = hardwareMap.servo.get("gripper");
         wrist = hardwareMap.servo.get("wrist");
 
+        /*
+            We are using Actuonix PQ-12 linear actuators for the grippers as of Dec 12 2019. They
+            take a signal between 1.0 milliseconds (extend; 1000 microseconds) and 2.0 milliseconds
+            (retract; 2000 microseconds) according to Actuonix's datasheet. REV Smart Robot Servos,
+            for comparison, take a signal between 500 microseconds (-90 degrees) and 2500
+            microseconds (90 degrees), so a 2500 microseconds signal is sent when setPosition(1.0)
+            is called. To correct the range for the linear actuator servos, We are setting them to
+            0.4 (1000 / 2500) and 0.8 (2000 / 2500).
+         */
+        gripper.scaleRange(0.4, 0.8);
+
         telemetry.addLine("Ready");
         telemetry.update();
 
