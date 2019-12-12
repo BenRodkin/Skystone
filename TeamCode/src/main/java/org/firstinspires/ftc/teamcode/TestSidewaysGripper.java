63e6358ecdd7dc0e2a0f62333653ab6b31eaf965
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-12-12 13:23:47 -0500

    Created TestSidewaysGripper testing OpMode
    This OpMode will let us test our new gripper. Servo hardware members will be added to the hardware class once testing completes.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestSidewaysGripper.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name = "Test: Sideways Gripper", group = "Testing")
+public class TestSidewaysGripper extends LinearOpMode {
+
+    // Hardware class
+    SlippyBotHardware hardware = new SlippyBotHardware();
+
+    // Sideways gripper hardware
+    Servo grip;     // Grips the Stone. This object controls both linear actuators at once because they are wired with a Y-splitter.
+    Servo wrist;    // Controls the rotation of the gripping mechanism.
+
+    @Override
+    public void runOpMode() {
+
+        // Initialize hardware
+        hardware.init(hardwareMap);
+
+        // Initialize sideways gripper hardware
+        grip = hardwareMap.servo.get("gripper");
+        wrist = hardwareMap.servo.get("wrist");
+
+        telemetry.addLine("Ready");
+        telemetry.update();
+
+        waitForStart();
+
+        while(opModeIsActive()) {
+
+
+            telemetry.addLine("Running");
+            telemetry.addLine();
+            telemetry.addData("Grip position", grip.getPosition());
+            telemetry.addData("Wrist position", wrist.getPosition());
+            telemetry.addLine();
+            telemetry.addData("Arm position", hardware.arm.getCurrentPosition());
+            telemetry.update();
+        }
+    }
+}
