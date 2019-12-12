bba71beec415554841cf2559fd3c93fab4512a9d
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-12-12 17:40:14 -0500

    Deactivated clamp controls and telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SlippyBotTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SlippyBotTeleOp.java
@@ -16,7 +16,7 @@ public class SlippyBotTeleOp extends OpMode {
     double pullyPower = 0.0;
 
     double armPower = 0.0;
-    double clampPos = 0.0;
+//    double clampPos = 0.0;
 
     GamepadCooldowns gp2 = new GamepadCooldowns();
     double runtime = 0.0;
@@ -61,6 +61,11 @@ public class SlippyBotTeleOp extends OpMode {
             hardware.intakeRight.setPower(0.0);
         }
 
+//        if(gamepad2.a && gp2.a.ready(runtime)) {
+//            clampPos = Math.abs(1 - clampPos);
+//            gp2.a.updateSnapshot(runtime);
+//        }
+
         if(gamepad2.a && gp2.a.ready(runtime)) {
             clampPos = Math.abs(1 - clampPos);
             gp2.a.updateSnapshot(runtime);
@@ -79,14 +84,14 @@ public class SlippyBotTeleOp extends OpMode {
         hardware.pulleyRight.   setPower(pullyPower);
 
         hardware.arm.           setPower(armPower);
-        hardware.clamp.         setPosition(clampPos);
+//        hardware.clamp.         setPosition(clampPos);
 
 
         telemetry.addLine("Running");
         telemetry.addLine();
         telemetry.addData("Arm position", hardware.arm.getCurrentPosition());
         telemetry.addLine();
-        telemetry.addData("Clamp position", hardware.clamp.getPosition());
+//        telemetry.addData("Clamp position", hardware.clamp.getPosition());
         telemetry.addLine();
         telemetry.addData("Left pulley position", hardware.pulleyLeft.getCurrentPosition());
         telemetry.addData("Right pulley position", hardware.pulleyRight.getCurrentPosition());
