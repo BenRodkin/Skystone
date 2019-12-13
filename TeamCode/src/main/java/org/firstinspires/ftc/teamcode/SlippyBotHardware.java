3a62a6b9f8999f37889ad97553a0040d5cea9beb
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-12-13 17:28:49 -0500

    Added motors and servos tags in hardware fields

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SlippyBotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SlippyBotHardware.java
@@ -14,6 +14,7 @@ import org.openftc.easyopencv.OpenCvInternalCamera;
 
 public class SlippyBotHardware {
 
+    // Motors
     public DcMotor frontLeft;
     public DcMotor frontRight;
     public DcMotor rearLeft;
@@ -24,6 +25,7 @@ public class SlippyBotHardware {
 
     public DcMotor arm;
 
+    // Servos
     public CRServo intakeLeft;
     public CRServo intakeRight;
 
