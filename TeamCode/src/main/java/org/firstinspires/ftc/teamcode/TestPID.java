0549c8fe4e2160d7c98a7aa1d9fc6b1b9441ea84
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-12-13 17:28:12 -0500

    Deactivated local IMU setup

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestPID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestPID.java
@@ -17,7 +17,7 @@ import static org.firstinspires.ftc.teamcode.SlippyBotHardware.TIMEOUT;
 public class TestPID extends LinearOpMode {
 
     // IMU
-    BNO055IMU imu;
+//    BNO055IMU imu;
 
     // Hardware class
     SlippyBotHardware hardware = new SlippyBotHardware();
@@ -44,16 +44,16 @@ public class TestPID extends LinearOpMode {
         telemetry.addLine("Initializing hardware");
         telemetry.update();
 
-        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
-        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
-        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
-        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
-        parameters.loggingEnabled      = true;
-        parameters.loggingTag          = "IMU";
-        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
-
-        imu = hardwareMap.get(BNO055IMU.class, "imu");
-        imu.initialize(parameters);
+//        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+//        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+//        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+//        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+//        parameters.loggingEnabled      = true;
+//        parameters.loggingTag          = "IMU";
+//        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+//
+//        imu = hardwareMap.get(BNO055IMU.class, "imu");
+//        imu.initialize(parameters);
 
         hardware.init(hardwareMap);
 
