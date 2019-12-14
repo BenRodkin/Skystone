7c8e30223d8e738dedda4d43e76656f35e5bfcab
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-12-13 20:20:44 -0500

    Added methods for clamping and releasing Foundation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestFoundation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestFoundation.java
@@ -12,6 +12,12 @@ public class TestFoundation extends OpMode {
     Servo found_left;
     Servo found_right;
 
+    private final double CLAMP_LEFT     = 0.0;
+    private final double RELEASE_LEFT   = 1.0;
+
+    private final double CLAMP_RIGHT    = 1.0;
+    private final double RELEASE_RIGHT  = 0.0;
+
 
 
     public void init() {
@@ -32,5 +38,16 @@ public class TestFoundation extends OpMode {
         telemetry.addData("found_left position", found_left.getPosition());
         telemetry.addData("found_right position", found_right.getPosition());
         telemetry.update();
+    }
+
+
+    public void clampFoundation() {
+        found_left.setPosition(CLAMP_LEFT);
+        found_right.setPosition(CLAMP_RIGHT);
+    }
+
+    public void releaseFoundation() {
+        found_left.setPosition(RELEASE_LEFT);
+        found_right.setPosition(RELEASE_RIGHT);
     }
 }
