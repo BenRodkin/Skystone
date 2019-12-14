d26e93ed6978e608103a780ff90b0ce450ac6d66
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-12-13 20:20:26 -0500

    Added found_left and found_right Foundation clamping servos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestFoundation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestFoundation.java
@@ -2,14 +2,23 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "Test: Foundation grabbers", group = "Testing")
 public class TestFoundation extends OpMode {
 
     SlippyBotHardware hardware = new SlippyBotHardware();
 
+    Servo found_left;
+    Servo found_right;
+
+
+
     public void init() {
         hardware.init(hardwareMap); // Default to false for camera and IMU
+
+        found_left  = hardwareMap.servo.get("found_left");
+        found_right = hardwareMap.servo.get("found_right");
 
         telemetry.addLine("Ready");
         telemetry.update();
@@ -19,6 +28,9 @@ public class TestFoundation extends OpMode {
 
 
         telemetry.addLine("Running");
+        telemetry.addLine();
+        telemetry.addData("found_left position", found_left.getPosition());
+        telemetry.addData("found_right position", found_right.getPosition());
         telemetry.update();
     }
 }
