9bdebe2655a565969cbdc47c58db0e22570a8465
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-12-29 14:49:22 -0500

    Added import statements to accommodate packaging

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SlippyBotTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SlippyBotTeleOp.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.Miscellaneous.GamepadCooldowns;
+
 import static org.firstinspires.ftc.teamcode.SlippyBotHardware.FAST;
 import static org.firstinspires.ftc.teamcode.SlippyBotHardware.SLOW;
 import static org.firstinspires.ftc.teamcode.SlippyBotHardware.WRIST_GRABBING;
