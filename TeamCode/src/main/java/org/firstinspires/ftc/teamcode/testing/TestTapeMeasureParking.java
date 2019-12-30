78268fca375ef08b2917257338e647f7138728d4
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-12-30 17:31:26 -0500

    Created new class to test the tape measure

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/TestTapeMeasureParking.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.testing;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Test: Tape Measure", group = "Testing")
+public class TestTapeMeasureParking extends OpMode {
+    @Override
+    public void init() {
+    }
+
+    @Override
+    public void loop() {
+    }
+}
