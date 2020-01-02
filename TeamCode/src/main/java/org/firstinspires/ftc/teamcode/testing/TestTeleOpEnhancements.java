5270d674f867279e9193d3d76054708b0298b04b
Blake Sanders <ac3.train3r.blak3@gmail.com>
2020-01-02 17:42:50 -0500

    Removed wrist scalar controls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/TestTeleOpEnhancements.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/TestTeleOpEnhancements.java
@@ -15,9 +15,6 @@ public class TestTeleOpEnhancements extends OpMode {
     private final boolean INIT_CAM = false;
     private final boolean INIT_IMU = true;
 
-    private double wristScalarLocal = 0.008;
-    private final double WRIST_STEP = 0.0005;
-
     GamepadCooldowns gp2 = new GamepadCooldowns();
     double runtime = 0.0;
 
@@ -38,28 +35,13 @@ public class TestTeleOpEnhancements extends OpMode {
 
         hardware.mecanumDriveFieldCentric(gamepad1.left_stick_x, -gamepad1.left_stick_y, -gamepad1.right_stick_x, hardware.heading());
 
-        // Dpad up increases wrist scalar
         hardware.wrist.setPosition(hardware.wrist.getPosition() + (gamepad2.right_stick_y * WRIST_SCALAR));
-
-        if(gamepad2.dpad_up && gp2.dpUp.ready(runtime)) {
-            wristScalarLocal += WRIST_STEP;
-
-            gp2.dpUp.updateSnapshot(runtime);
-        }
-
-        // Dpad down decreases wrist scalar
-        if(gamepad2.dpad_down && gp2.dpDown.ready(runtime)){
-            wristScalarLocal -= WRIST_STEP;
-
-            gp2.dpDown.updateSnapshot(runtime);
-        }
 
         telemetry.addLine("Running");
         telemetry.addLine();
         telemetry.addData("Heading", hardware.heading());
         telemetry.addLine();
         telemetry.addData("Wrist pos", hardware.wrist.getPosition());
-        telemetry.addData("Wrist scalar (local)", wristScalarLocal);
         telemetry.update();
     }
 }
