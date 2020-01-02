1e2dd1fb38b8a39e69326a6023731a43f1b7eddb
Blake Sanders <ac3.train3r.blak3@gmail.com>
2020-01-02 17:26:56 -0500

    Added wrist scalar controls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/TestTeleOpEnhancements.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/TestTeleOpEnhancements.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.SlippyBotHardware;
+import org.firstinspires.ftc.teamcode.miscellaneous.GamepadCooldowns;
 
 import static org.firstinspires.ftc.teamcode.SlippyBotHardware.WRIST_SCALAR;
 
@@ -15,9 +16,17 @@ public class TestTeleOpEnhancements extends OpMode {
     private final boolean INIT_IMU = true;
 
     private double wristScalarLocal = 0.008;
+    private final double WRIST_STEP = 0.0005;
+
+    GamepadCooldowns gp2 = new GamepadCooldowns();
+    double runtime = 0.0;
+
 
     public void init() {
         hardware.init(hardwareMap, INIT_CAM, INIT_IMU);
+
+        gp2.dpUp.setCooldown(0.500);    // 500ms
+        gp2.dpDown.setCooldown(0.500);  // 500ms
 
         telemetry.addLine("Ready");
         telemetry.update();
@@ -25,9 +34,23 @@ public class TestTeleOpEnhancements extends OpMode {
 
     public void loop() {
 
+        runtime = getRuntime();
+
         hardware.mecanumDriveFieldCentric(gamepad1.left_stick_x, -gamepad1.left_stick_y, -gamepad1.right_stick_x, hardware.heading());
 
+        // Dpad up increases wrist scalar
+        if(gamepad2.dpad_up && gp2.dpUp.ready(runtime)) {
+            wristScalarLocal += WRIST_STEP;
 
+            gp2.dpUp.updateSnapshot(runtime);
+        }
+
+        // Dpad down decreases wrist scalar
+        if(gamepad2.dpad_down && gp2.dpDown.ready(runtime)){
+            wristScalarLocal -= WRIST_STEP;
+
+            gp2.dpDown.updateSnapshot(runtime);
+        }
         hardware.wrist.setPosition(hardware.wrist.getPosition() + (gamepad2.right_stick_y * wristScalarLocal));
 
         telemetry.addLine("Running");
