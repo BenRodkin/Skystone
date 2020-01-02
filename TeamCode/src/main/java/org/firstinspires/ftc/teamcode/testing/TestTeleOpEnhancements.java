fc01c3a49be5bf6844e3e77818c61f56fc89454c
Blake Sanders <ac3.train3r.blak3@gmail.com>
2020-01-02 16:45:32 -0500

    Added field-centric mecanum drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/TestTeleOpEnhancements.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/TestTeleOpEnhancements.java
@@ -22,9 +22,65 @@ public class TestTeleOpEnhancements extends OpMode {
 
     public void loop() {
 
+        mecanumDriveFieldCentric(gamepad1.left_stick_x, -gamepad1.left_stick_y, -gamepad1.right_stick_x, hardware.heading());
+
+
         telemetry.addLine("Running");
         telemetry.addLine();
         telemetry.addData("Heading", hardware.heading());
         telemetry.update();
+    }
+
+
+    public void mecanumDriveFieldCentric(double x, double y, double twist, double heading) {
+        // Account for the gyro heading in the drive vector
+        double[] rotated = rotateVector(x, y, -1 * heading);    // Heading is negated to correct vector rotation
+
+        // We are making seperate variables here for clarity's sake
+        double xIn = rotated[0];
+        double yIn = rotated[1];
+
+        // Put motor powers in an array to be normalized to largest value
+        double[] motorSpeeds = {
+                yIn + xIn + twist,  // Front left
+                yIn - xIn - twist,  // Front right
+                yIn - xIn + twist,  // Rear left
+                yIn + xIn - twist   // Rear right
+        };
+
+        // Normalize so largest speed is 1.0
+        normalize(motorSpeeds);
+
+
+        // Set motor powers
+        hardware.frontLeft  .setPower(motorSpeeds[0]);
+        hardware.frontRight .setPower(motorSpeeds[1]);
+        hardware.rearLeft   .setPower(motorSpeeds[2]);
+        hardware.rearRight  .setPower(motorSpeeds[3]);
+    }
+
+    protected static double[] rotateVector(double x, double y, double angle) {
+        double cosA = Math.cos(angle * (Math.PI / 180.0));
+        double sinA = Math.sin(angle * (Math.PI / 180.0));
+        double[] out = new double[2];
+        out[0] = x * cosA - y * sinA;
+        out[1] = x * sinA + y * cosA;
+        return out;
+    }
+
+
+    protected static void normalize(double[] wheelSpeeds) {
+        double maxMagnitude = Math.abs(wheelSpeeds[0]);
+        for (int i = 1; i < wheelSpeeds.length; i++) {
+            double temp = Math.abs(wheelSpeeds[i]);
+            if (maxMagnitude < temp) {
+                maxMagnitude = temp;
+            }
+        }
+        if (maxMagnitude > 1.0) {
+            for (int i = 0; i < wheelSpeeds.length; i++) {
+                wheelSpeeds[i] /= maxMagnitude;
+            }
+        }
     }
 }
