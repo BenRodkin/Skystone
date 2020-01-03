919f2713e0cc5970bed8df81589d6e177d555cd2
Blake Sanders <ac3.train3r.blak3@gmail.com>
2020-01-03 17:33:52 -0500

    Added toString() override for SkystonePlacement enum

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Miscellaneous/SkystonePlacement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Miscellaneous/SkystonePlacement.java
@@ -3,5 +3,15 @@ package org.firstinspires.ftc.teamcode.miscellaneous;
 public enum SkystonePlacement {
     LEFT,
     CENTER,
-    RIGHT
+    RIGHT;
+
+    @Override
+    public String toString() {
+        switch(this) {
+            case LEFT:      return "Left";
+            case CENTER:    return "Center";
+            case RIGHT:     return "Right";
+            default:        return "[Defaulted]";
+        }
+    }
 }
