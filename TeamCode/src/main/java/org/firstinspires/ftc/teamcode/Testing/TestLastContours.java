0d62410630907ebc8f2ed530e94a650f2b97b485
Blake Sanders <ac3.train3r.blak3@gmail.com>
2020-01-18 18:08:58 -0500

    Disabled TestLastContours and TestAutoBlueQuarry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/TestLastContours.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/TestLastContours.java
@@ -1,52 +1,52 @@
-
-/**
- * Write a description of class data_test here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
-import java.util.*;
-public class lastContours
-{
-    private List<Double> lastVals = new ArrayList<Double>();
-    private int numValues;
-    
-    public lastContours(int numValues){
-        this.numValues = numValues;
-    }
-    
-    public void add(double d) {
-        if(lastVals.size() == numValues) {
-            for(int i = 1; i < lastVals.size(); i++) {
-                lastVals.set(i-1, lastVals.get(i));
-            }
-            lastVals.set(lastVals.size()-1,d);
-        } else {
-            lastVals.add(d);
-        }
-    }
-    
-    public int getNumValues() {
-        return numValues;
-    }
-    
-    public double[] getVals() {
-        double[] temp = new double[lastVals.size()];
-        for(int i = 0; i < lastVals.size(); i++) {
-            temp[i] = lastVals.get(i);
-        }
-        return temp;
-    }
-    
-    public double getAverage() {
-        double total = 0.0;
-        for(Double d : lastVals) {
-            total += d;
-        }
-        return total/lastVals.size();
-    }
-    
-    public void clearData() {
-        lastVals.clear();
-    }
-}
+//
+///**
+// * Write a description of class data_test here.
+// *
+// * @author (your name)
+// * @version (a version number or a date)
+// */
+//import java.util.*;
+//public class lastContours
+//{
+//    private List<Double> lastVals = new ArrayList<Double>();
+//    private int numValues;
+//
+//    public lastContours(int numValues){
+//        this.numValues = numValues;
+//    }
+//
+//    public void add(double d) {
+//        if(lastVals.size() == numValues) {
+//            for(int i = 1; i < lastVals.size(); i++) {
+//                lastVals.set(i-1, lastVals.get(i));
+//            }
+//            lastVals.set(lastVals.size()-1,d);
+//        } else {
+//            lastVals.add(d);
+//        }
+//    }
+//
+//    public int getNumValues() {
+//        return numValues;
+//    }
+//
+//    public double[] getVals() {
+//        double[] temp = new double[lastVals.size()];
+//        for(int i = 0; i < lastVals.size(); i++) {
+//            temp[i] = lastVals.get(i);
+//        }
+//        return temp;
+//    }
+//
+//    public double getAverage() {
+//        double total = 0.0;
+//        for(Double d : lastVals) {
+//            total += d;
+//        }
+//        return total/lastVals.size();
+//    }
+//
+//    public void clearData() {
+//        lastVals.clear();
+//    }
+//}
